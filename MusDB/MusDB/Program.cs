1a3feaca5517b34f708a77b96af42460bf6e8de0
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-18 18:20:57 +0800

    ~~

--- MusDB/MusDB/Program.cs
+++ MusDB/MusDB/Program.cs
@@ -31,75 +31,13 @@ namespace MusDB
             CLI.Line($"当前数据库记录存留：{result}");
             CLI.Pause("\a按任意键收集数据");
 
-            (int flac, int mp3, int etc) = (0, 0, 0);
-            int total = 0;
-
             List<FileInfo> ETC = new();
             List<(string name, string md5)> Music = new();
 
-            (string, string) ToSHA256(string path)
-            {
-                using SHA256 SHA256 = SHA256.Create();
-                using FileStream File = new FileStream(path, FileMode.Open, FileAccess.Read);
-
-                string result = BitConverter.ToString(SHA256.ComputeHash(File));
-
-                return new(path, result);
-            }
+            Checker.CheckFiles(@"D:\Thaumy的乐库\Playlists\.喵喵喵", out (int flac, int mp3, int etc, int total) Count);
 
 
-            void fun(string path)
-            {
-                foreach (var el in new DirectoryInfo(path).GetFileSystemInfos())
-                {
-                    if (el is DirectoryInfo)
-                    {
-                        fun(el.FullName);
-                    }
-                    else
-                    {
-                        CLI.Put(total.ToString().PadLeft(4, '0'));
-                        var temp = (FileInfo)el;
-                        if (temp.Name.Contains(".flac"))
-                        {
-                            CLI.Line(" | flac  " + temp.Name);
-                            flac++;
-
-                            var md5 = ToSHA256(temp.FullName);
-                            Music.Add(md5);
-                            DB.Update(temp.Name, md5.Item2, "flac");
-
-                            CLI.InPosition(Console.WindowWidth / 5 * 3, Console.CursorTop - 1,
-                               () => { CLI.Line(temp.DirectoryName); });
-                        }
-                        else if (temp.Name.Contains(".mp3"))
-                        {
-                            CLI.Line(" |  mp3  " + temp.Name);
-                            mp3++;
-
-                            var md5 = ToSHA256(temp.FullName);
-                            Music.Add(md5);
-                            DB.Update(temp.Name, md5.Item2, "mp3");
-
-                            CLI.InPosition(Console.WindowWidth / 5 * 3, Console.CursorTop - 1,
-                               () => { CLI.Line(temp.DirectoryName); });
-                        }
-                        else
-                        {
-                            CLI.Line(temp.Name);
-                            ETC.Add(temp);
-                            etc++;
-                        }
-                        total++;
-                    }
-
-                }
-            }
-
-            fun(@"D:\Thaumy的乐库\Playlists\.喵喵喵");
-
-
-            CLI.Line($"\nflac:{flac}  mp3:{mp3}  其他:{etc}\n");
+            CLI.Line($"\nflac:{Count.flac}  mp3:{Count.mp3}  其他:{Count.etc}\n");
 
 
             foreach (var el in ETC)
@@ -107,7 +45,7 @@ namespace MusDB
                 CLI.Line(el.FullName);
             }
 
-            Conflict.Check(Music);
+            Checker.CheckConflict(Music);
 
             CLI.InColor(ConsoleColor.Green, () => CLI.Pause("\n\a检查完成。"));
         }
