ec58575cc7c1691efc145e9ecd3f879a12beb08e
Thaumy Cheng <Studio_ai@outlook.com>
2021-01-18 12:46:25 +0800

    ~~

---
+++ MusDB/MusDB/Database.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WaterLibrary.MySQL;
+
+using MySql.Data.MySqlClient;
+using MySql;
+using MusDB;
+using WaterLibrary.Util;
+using System.Security.Cryptography;
+
+namespace MusDB
+{
+    class Database
+    {
+        private readonly MySqlManager MySqlManager;
+
+        public Database(string User, string PWD, string Database)
+        {
+            MySqlManager = new MySqlManager(new("localhost", 3306, User, PWD), Database);
+        }
+
+        public int GetCount()
+        {
+            return Convert.ToInt32(MySqlManager.GetKey("SELECT COUNT(*) FROM statistics"));
+        }
+
+        public void Update(string Name, string MD5, string file_type)
+        {
+            MySqlManager.DoInConnection(conn =>
+            {
+                using MySqlCommand MySqlCommand = new MySqlCommand
+                {
+                    CommandText = string.Format("INSERT INTO statistics (name,md5,file_type) VALUES (\"{0}\",\"{1}\",\"{2}\");",
+                    Name, MD5, file_type
+                    ),
+
+                    Connection = conn,
+
+                    /* 开始事务 */
+                    Transaction = conn.BeginTransaction()
+                };
+
+                if (MySqlCommand.ExecuteNonQuery() == 1)
+                {
+                    /* 指向表修改1行数据，拷贝表删除1行数据 */
+                    MySqlCommand.Transaction.Commit();
+                    return true;
+                }
+                else
+                {
+                    MySqlCommand.Transaction.Rollback();
+                    return false;
+                }
+            });
+        }
+    }
+}
--- MusDB/MusDB/Program.cs
+++ MusDB/MusDB/Program.cs
@@ -18,19 +18,18 @@ namespace MusDB
     {
         static void Main(string[] args)
         {
-
             CLI.Line("初始化MusDB数据库服务..................[ ]");
-            var MySqlManager = new MySqlManager(new("localhost", 3306, "root", "65a1561425f744e2b541303f628963f8"), "musdb");
+            Database DB = new("root", "65a1561425f744e2b541303f628963f8", "musdb");
             CLI.InPosition(40, Console.CursorTop - 1,
                         () => { CLI.InColor(ConsoleColor.Green, () => Console.WriteLine("O")); });
 
             CLI.Line("统计信息...............................[ ]");
-            var result = MySqlManager.GetKey("SELECT COUNT(*) FROM statistics");
+            var result = DB.GetCount();
             CLI.InPosition(40, Console.CursorTop - 1,
                        () => { CLI.InColor(ConsoleColor.Green, () => Console.WriteLine("O")); });
 
             CLI.Line($"当前数据库记录存留：{result}");
-            CLI.Pause("按任意键收集数据");
+            CLI.Pause("\a按任意键收集数据");
 
             (int flac, int mp3, int etc) = (0, 0, 0);
             int total = 0;
@@ -48,35 +47,7 @@ namespace MusDB
                 return new(path, result);
             }
 
-            void db(string name, string md5, string file_type)
-            {
-                MySqlManager.DoInConnection(conn =>
-                {
-                    using MySqlCommand MySqlCommand = new MySqlCommand
-                    {
-                        CommandText = string.Format("INSERT INTO statistics (name,md5,file_type) VALUES (\"{0}\",\"{1}\",\"{2}\");",
-                        name, md5, file_type
-                        ),
 
-                        Connection = conn,
-
-                        /* 开始事务 */
-                        Transaction = conn.BeginTransaction()
-                    };
-
-                    if (MySqlCommand.ExecuteNonQuery() == 1)
-                    {
-                        /* 指向表修改1行数据，拷贝表删除1行数据 */
-                        MySqlCommand.Transaction.Commit();
-                        return true;
-                    }
-                    else
-                    {
-                        MySqlCommand.Transaction.Rollback();
-                        return false;
-                    }
-                });
-            }
             void fun(string path)
             {
                 foreach (var el in new DirectoryInfo(path).GetFileSystemInfos())
@@ -96,7 +67,7 @@ namespace MusDB
 
                             var md5 = ToSHA256(temp.FullName);
                             Music.Add(md5);
-                            db(temp.Name, md5.Item2, "flac");
+                            DB.Update(temp.Name, md5.Item2, "flac");
 
                             CLI.InPosition(Console.WindowWidth / 5 * 3, Console.CursorTop - 1,
                                () => { CLI.Line(temp.DirectoryName); });
@@ -108,7 +79,7 @@ namespace MusDB
 
                             var md5 = ToSHA256(temp.FullName);
                             Music.Add(md5);
-                            db(temp.Name, md5.Item2, "mp3");
+                            DB.Update(temp.Name, md5.Item2, "mp3");
 
                             CLI.InPosition(Console.WindowWidth / 5 * 3, Console.CursorTop - 1,
                                () => { CLI.Line(temp.DirectoryName); });
@@ -128,30 +99,17 @@ namespace MusDB
             fun(@"D:\Thaumy的乐库\Playlists\.喵喵喵");
 
 
-            CLI.Line();
-            CLI.Line($"flac:{flac}  mp3:{mp3}  其他:{etc}");
-            CLI.Line();
+            CLI.Line($"\nflac:{flac}  mp3:{mp3}  其他:{etc}\n");
+
 
             foreach (var el in ETC)
             {
                 CLI.Line(el.FullName);
             }
 
-            CLI.Line("冲突项目：");
+            Conflict.Check(Music);
 
-            var conflicts = from el in (from el in Music group el by el.md5) where el.Count() > 1 select el;
-
-            foreach (var el in conflicts)
-            {
-                foreach (var it in el)
-                {
-                    CLI.Line(it.name);
-                }
-                CLI.Line();
-
-            }
-
-            CLI.Pause("检查完成。");
+            CLI.InColor(ConsoleColor.Green, () => CLI.Pause("\n\a检查完成。"));
         }
     }
 }
