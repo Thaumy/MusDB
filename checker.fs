f566d325cfb32f3bc658acd1d20e7fa6c4cc8c1c
Thaumy <Studio_ai@outlook.com>
2022-03-02 08:04:03 +0800

    beautified code style

--- checker.fs
+++ checker.fs
@@ -10,68 +10,67 @@ open types
 open ui
 
 let showMusic musicName isFlac musicPath =
-    let (text, color) =
+    let text, color =
         if isFlac then
             ("FLAC", ConsoleColor.Cyan)
         else
             ("MP3 ", ConsoleColor.White)
 
-    CLI.InColor ConsoleColor.DarkGray (fun _ -> CLI.Put "|")
-    CLI.InColor color (fun _ -> CLI.Put $" {text} ")
-    CLI.InColor ConsoleColor.DarkGray (fun _ -> CLI.Put "|")
-    CLI.Put $"{musicName}"
-    CLI.InColor ConsoleColor.DarkGray (fun _ -> CLI.InRight musicPath)
-    CLI.newLine
+    inColor ConsoleColor.DarkGray (fun _ -> put "|")
+    inColor color (fun _ -> put $" {text} ")
+    inColor ConsoleColor.DarkGray (fun _ -> put "|")
+    put $"{musicName}"
+    inColor ConsoleColor.DarkGray (fun _ -> inRight musicPath)
+    newLine ()
 
-type Checker =
-    static member ToSha256 path =
-        let file =
-            new FileStream(path, FileMode.Open, FileAccess.Read)
+let toSha256 path =
+    let file =
+        new FileStream(path, FileMode.Open, FileAccess.Read)
 
-        SHA256.Create()
-        |> fun it -> it.ComputeHash file |> BitConverter.ToString
+    SHA256.Create()
+    |> fun it -> it.ComputeHash file |> BitConverter.ToString
 
-    static member GetFileSystemInfosList path =
-        map
-            id
-            [ for x in (new DirectoryInfo(path)).GetFileSystemInfos() do
-                  x ]
+let getFileSystemInfosList path =
+    map
+        id
+        [ for x in ( DirectoryInfo(path)).GetFileSystemInfos() do
+              x ]
 
-    static member GetAllFiles(list: FileSystemInfo list) =
-        match list with
-        | x :: xs ->
-            match x with
-            | :? DirectoryInfo as di ->
-                Checker.GetAllFiles(Checker.GetFileSystemInfosList di.FullName)
-                @ Checker.GetAllFiles xs
-            | :? FileInfo as fi ->
-                match true with
-                | _ when fi.Name.Contains ".flac" ->
-                    showMusic fi.Name true fi.DirectoryName
+let rec getAllFiles (list: FileSystemInfo list) =
+    match list with
+    | x :: xs ->
+        match x with
+        | :? DirectoryInfo as di ->
+            getAllFiles (getFileSystemInfosList di.FullName)
+            @ getAllFiles xs
+        | :? FileInfo as fi ->
+            match true with
+            | _ when fi.Name.Contains ".flac" ->
+                showMusic fi.Name true fi.DirectoryName
 
-                    { Name = fi.Name
-                      Sha256 = Checker.ToSha256(fi.FullName)
-                      Path = fi.DirectoryName
-                      Type = "flac" }
-                    :: Checker.GetAllFiles xs
-                | _ when fi.Name.Contains ".mp3" ->
-                    showMusic fi.Name false fi.DirectoryName
+                { Name = fi.Name
+                  Sha256 = toSha256 fi.FullName
+                  Path = fi.DirectoryName
+                  Type = "flac" }
+                :: getAllFiles xs
+            | _ when fi.Name.Contains ".mp3" ->
+                showMusic fi.Name false fi.DirectoryName
 
-                    { Name = fi.Name
-                      Sha256 = Checker.ToSha256(fi.FullName)
-                      Path = fi.DirectoryName
-                      Type = "mp3" }
-                    :: Checker.GetAllFiles xs
-                | _ ->
-                    { Name = fi.Name
-                      Sha256 = Checker.ToSha256(fi.FullName)
-                      Path = fi.DirectoryName
-                      Type = "" }
-                    :: Checker.GetAllFiles xs
-            | _ -> []
-        | [] -> []
+                { Name = fi.Name
+                  Sha256 = toSha256 fi.FullName
+                  Path = fi.DirectoryName
+                  Type = "mp3" }
+                :: getAllFiles xs
+            | _ ->
+                { Name = fi.Name
+                  Sha256 = toSha256 fi.FullName
+                  Path = fi.DirectoryName
+                  Type = "" }
+                :: getAllFiles xs
+        | _ -> []
+    | [] -> []
 
-    static member CheckConflicts(files: File list) =
-        [ for el in files.GroupBy(fun el -> el.Sha256) do
-              if el.Count() > 1 then
-                  for it in el -> it ]
+let checkConflicts (files: File list) =
+    [ for el in files.GroupBy(fun el -> el.Sha256) do
+          if el.Count() > 1 then
+              for it in el -> it ]
--- ui.fs
+++ ui.fs
@@ -3,34 +3,31 @@
 open System
 open System.Text
 
-type CLI =
+let inColor color todo : unit =
+    Console.ForegroundColor <- color
+    todo ()
+    Console.ForegroundColor <- ConsoleColor.White
 
-    static member InColor color todo : unit =
-        Console.ForegroundColor <- color
-        todo ()
-        Console.ForegroundColor <- ConsoleColor.White
+let inPosition L R todo : unit =
+    Console.SetCursorPosition(L, R)
+    todo ()
 
+let inRight (text: string) : unit =
+    Console.SetCursorPosition(
+        Console.WindowWidth
+        - Encoding.Default.GetByteCount(text),
+        Console.CursorTop
+    )
 
-    static member InPosition L R todo : unit =
-        Console.SetCursorPosition(L, R)
-        todo ()
+    Console.Write text
 
-    static member InRight(text: string) : unit =
-        Console.SetCursorPosition(
-            Console.WindowWidth
-            - Encoding.Default.GetByteCount(text),
-            Console.CursorTop
-        )
-
-        Console.Write text
-
-    static member Pause(text: 'T) =
-        Console.Write text
-        Console.ReadLine()
+let pause (text: 'T) =
+    Console.Write text
+    Console.ReadLine()
 
 
-    static member Put(text: 'T) : unit = Console.Write text
+let put (text: 'T) : unit = Console.Write text
 
-    static member Line(text: 'T) : unit = Console.WriteLine text
+let line (text: 'T) : unit = Console.WriteLine text
 
-    static member newLine: unit = Console.WriteLine ""
+let newLine () = Console.WriteLine ""
